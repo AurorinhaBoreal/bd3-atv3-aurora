92e1194750e6f4dea4fca85ea6afbbe1e41f5651
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-12-03 21:40:48 -0300

    feat: Style Student Card

--- src/components/StudentCard/index.tsx
+++ src/components/StudentCard/index.tsx
@@ -5,28 +5,68 @@ interface info {
     student: Student
 }
 const StudentCard = ({student}: info) => {
+    
+    const formatBirthDate = (birthDate: string) => {
+        let formattedBirthDate;
+        
+        formattedBirthDate = birthDate.replace(/-/g, "/")
+        return formattedBirthDate;
+    }
+
+    const formatCPF = (cpf: string) => {
+        return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+    }
+
+    const formatRG = (rg: string) => {
+        return rg.replace(/(\d{2})(\d{3})(\d{3})(\d{1,2})/, "$1.$2.$3-$4");
+    }
+
+    const formatPhoneNumber = (phoneNumber: string) => {
+        return phoneNumber.replace(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3");
+    }
+
     return (
         <div className={styles.cardBody}>
             <div className={styles.studentInfo}>
                 <h5 className={styles.infoTitle}>Student</h5>
                 <div className={styles.firstLine}>
                     <p>{student.firstName} {student.lastName}</p>
-                    <p>{student.birthDate}</p>
+                    <p>{formatBirthDate(student.birthDate)}</p>
                 </div>
                 <div className={styles.secondLine}>
-                    <p>{student.cpf}</p>
-                    <p>{student.rg}</p>
+                    <div>
+                        <h5 className={styles.infoName}>CPF</h5>
+                        <p>{formatCPF(student.cpf)}</p>
+                    </div>
+                    <div>
+                        <h5 className={styles.infoName}>RG</h5>
+                        <p>{formatRG(student.rg)}</p>
+                    </div>
                 </div>
                 <div className={styles.thirdLine}>
-                    <p>{student.email}</p>
-                    <p>{student.studentNumber}</p>
+                    <div>
+                        <h5 className={styles.infoName}>Phone Number</h5>
+                        <p className={styles.smallInfo}>{formatPhoneNumber(student.studentNumber)}</p>
+                    </div>
+                    <div>
+                        <h5 className={styles.infoName}>Email</h5>
+                        <p className={styles.smallInfo}>{student.email}</p>
+                    </div>
                 </div>
             </div>
             <hr className={styles.infoDivider}/>
             <div className={styles.parentInfo}>
                 <h5 className={styles.infoTitle}>Parent</h5>
-                <p>{student.parentName}</p>
-                <p>{student.parentNumber}</p>
+                <div className={styles.alignedInfo}>
+                    <div>
+                        <h5 className={styles.infoName}>Parent Name</h5>
+                        <p>{student.parentName}</p>
+                    </div>
+                    <div>
+                        <h5 className={styles.infoName}>Parent Number</h5>
+                        <p>{formatPhoneNumber(student.parentNumber)}</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
